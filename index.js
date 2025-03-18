89c6e1d2e89a9492371e6c1d068e3ea1a25282a3
Simon <simonwarui93@gmail.com>
2025-03-18 04:31:41 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, events){
+    let message = [];
+    for (let i = 0; i < names.length; i++){
+    message.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`)
+    }
+    
+    return message
+    }
+    
+    function countDown(number) {
+    while (number >= 0) {
+    console.log(number);
+    number--;
+    }
+    }
+    
